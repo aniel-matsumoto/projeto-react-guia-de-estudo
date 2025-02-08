921a206e438b0d7531a53f8152fff8945edc945c
aniel-matsumoto <aniel.matsumoto.51@gmail.com>
2025-02-08 18:49:48 -0300

    comeco

---
+++ projeto-react/src/App.jsx
@@ -0,0 +1,14 @@
+import Titulo1 from "./components/Titulo1"
+
+function App() {
+  
+
+
+  return (
+  <>
+  <Titulo1/>
+  </>
+  )
+}
+
+export default App
---
+++ projeto-react/src/components/Titulo1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Titulo1() {
+  return (
+    <div>titulinho</div>
+  )
+}
+
+export default Titulo1
